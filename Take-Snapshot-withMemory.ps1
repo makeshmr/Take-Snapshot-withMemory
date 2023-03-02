1c7129c46ea3fb7fceabf8cd603996fa4fc96279
Makesh  Ramachandran <42498044+makeshmr@users.noreply.github.com>
2023-03-02 10:53:11 +0530

    Add files via upload

---
+++ Take-Snapshot-withMemory.ps1
@@ -0,0 +1,15 @@
+﻿function Take-Snapshot-withMemory {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Array]$VMList,
+        [Parameter(Mandatory = $true)]
+        [string]$Snapshot_Description
+
+
+
+        )
+        foreach ($VM in $VMList) {
+         New-Snapshot -VM $VM -Name $Snapshot_Description  -Description "Created $(Get-Date)" -Memory
+       }
+       }
